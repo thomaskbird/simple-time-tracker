04b8ae9d2262ccc047957ffcd7df03e5664858fc
Thomas Bird <tom.bird@nbcuni.com>
2023-05-15 23:24:08 -0400

    Removed client data from repo

--- src/config/sites.ts
+++ src/config/sites.ts
@@ -1,32 +1,5 @@
-import {ClientType, FilterType, SiteTypes} from '~/config/types';
-
-const clients: ClientType[] = [
-  {
-    id: 1,
-    name: 'Neurocrine',
-    code: 'NEUR0014/49',
-  },
-  {
-    id: 2,
-    name: 'AbbVie',
-    code: 'ABBV0053/01',
-  },
-  {
-    id: 3,
-    name: 'Internal - Onboarding',
-    code: 'SSC0042/04',
-  },
-  {
-    id: 4,
-    name: 'Prism Phase 2',
-    code: 'SSC0040/07'
-  },
-  {
-    id: 5,
-    name: 'New Amsterdam Pharma',
-    code: 'NewAm0006/04'
-  }
-];
+import {FilterType, SiteTypes} from '~/config/types';
+import {clients} from '~/mocks/client_mock';
 
 const filters: FilterType[] = [
   {
